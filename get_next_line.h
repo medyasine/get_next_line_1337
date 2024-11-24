3b7fe868f405aa68a4311699077eefa9b86f78e9
Mohamed yassine asnus <125496479+medyasine@users.noreply.github.com>
2024-11-24 11:09:31 +0100

    Update get_next_line.h

--- get_next_line.h
+++ get_next_line.h
@@ -12,22 +12,18 @@
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
-
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
 # include <stdlib.h>
 # include <unistd.h>
 # include <fcntl.h>
 
-# ifndef BUFFER_SIZE
-#  define BUFFER_SIZE 10
-# endif
+char    *get_next_line(int fd);
+char    *ft_strdup(char *s);
+size_t    ft_strlen(char *s);
+char    *ft_substr(char *s, unsigned int start, size_t len);
+char    *ft_strjoin(char *s1, char *s2);
+void    fill_str(char *res, char *s1, char *s2);
 
-char	*get_next_line(int fd);
-void	ft_readalloc(int fd, char **next);
-char	*ft_new_line(char *str);
-char	*ft_next(char *str, char **new_line);
-int		ft_strchr(char *s, int c);
-char	*ft_strjoin(char *s1, char *s2);
-void	*ft_calloc(size_t count, size_t size);
-void	ft_free(char **s1, char **s2);
-size_t	ft_strlen(char *s);
 #endif
